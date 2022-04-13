8a528506cf21d265db4bdab428ccf81174e571b2
Marcos Lima <48837929+MarcosACLima@users.noreply.github.com>
2022-04-13 11:48:04 -0300

    Buscando e removendo obj cozinha com EntityManager

--- src/main/java/com/dlima/jfood/jpa/BuscaCozinhaMain.java
+++ src/main/java/com/dlima/jfood/jpa/BuscaCozinhaMain.java
@@ -7,9 +7,10 @@ import org.springframework.context.ApplicationContext;
 import com.dlima.jfood.JfoodApiApplication;
 import com.dlima.jfood.domain.model.Cozinha;
 
-public class InclusaoCozinhaMain {
+public class BuscaCozinhaMain {
 
 	public static void main(String[] args) {
+		// Rodar como aplicação Java, nao web
 		ApplicationContext applicationContext = new SpringApplicationBuilder(JfoodApiApplication.class)
 				.web(WebApplicationType.NONE)
 				.run(args);
@@ -17,17 +18,9 @@ public class InclusaoCozinhaMain {
 		// Instancia de Cadastro Cozinha
 		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 
-		Cozinha cozinha1 = new Cozinha();
-		cozinha1.setNome("Brasileira");
+		Cozinha cozinha = cadastroCozinha.buscar(1L);
 		
-		Cozinha cozinha2 = new Cozinha();
-		cozinha2.setNome("Japonesa");
-		
-		cozinha1 = cadastroCozinha.salvar(cozinha1);
-		cozinha2 = cadastroCozinha.salvar(cozinha2);
-		
-		System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
-		System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
+		System.out.println(cozinha.getNome());
 		
 	}
 
