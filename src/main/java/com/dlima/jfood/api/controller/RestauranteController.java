f2021c3b87510f8b1d2d748cc5d0340f5f4f7744
Marcos Lima <48837929+MarcosACLima@users.noreply.github.com>
2022-04-29 22:29:30 -0300

    Tarefa: Modelando e implementando a atualização de recursos de restaurantes

--- src/main/java/com/dlima/jfood/api/controller/RestauranteController.java
+++ src/main/java/com/dlima/jfood/api/controller/RestauranteController.java
@@ -2,15 +2,16 @@ package com.dlima.jfood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dlima.jfood.domain.exception.EntidadeNaoEncontradaException;
@@ -55,5 +56,29 @@ public class RestauranteController {
 			return ResponseEntity.badRequest().body(e.getMessage()); // retornar mensagem que nao existe id de cozinha informado
  		}
 	}
-
+	
+	/* challenge */
+	@PutMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante) {
+		// Obs: necessario try / catch para EntidadeNaoEncontrada lancado BadResquest 
+		try {
+			// Buscar Objeto
+			Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+			
+			// Caso objeto encontrado
+			if (restauranteAtual != null) {
+				BeanUtils.copyProperties(restaurante, restauranteAtual, "id"); // ignorar ID ao salvar
+				
+				restauranteAtual = cadastroRestauranteService.salvar(restauranteAtual);
+				return ResponseEntity.ok(restauranteAtual); // response 200 com body
+			}
+			
+			// Caso nao exista o objeto/id informando - 404
+			return ResponseEntity.notFound().build();
+			
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
 }
